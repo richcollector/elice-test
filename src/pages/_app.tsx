ab4c49784c67a631ff8d658fc52354034ac24332
richcollector <ktw9115@naver.com>
2024-02-23 17:08:11 +0900

    style: styled-components 및 Layout 설정 GlobalStyle, ServerStyleSheet, favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
+import GlobalStyle from '@/common/styles/GlobalStyle';
+import Layout from '@/components/layout/Layout';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<GlobalStyle />
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</>
+	);
 }
